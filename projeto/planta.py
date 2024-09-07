a4db0611a8312f28209110c0e04fa3a91eeab7d2
José Alexandre da Silva França <alexandrefranca208@gmail.com>
2024-09-07 19:50:31 -0300

    comodo vazio

--- projeto/planta.py
+++ projeto/planta.py
@@ -23,14 +23,19 @@ simbols = dict(
 
 
 class Casa:
-    def __init__(self, tamanho = 68):
+    def __init__(self, width = 8, height = 9):
         #Armazena os andares
         self.andares = []
-        self.tamanho = tamanho
+        self.width = width
+        self.height = height
 
     def cria_andar(self, nome, listaComodos):
         novoAndar = Andar(nome,listaComodos)
         self.andares.append(novoAndar)
+
+    def generateHouse(self):
+        for i in range(len(self.andares)):
+            self.andares[i].createMap(self.width, self.height)
         
 
 class Andar:
@@ -42,8 +47,45 @@ class Andar:
         #Como os cômodos foram posicionados
         self.mapa= []
 
+
     def insereComodo(self, comodo):
         self.comodos.append(comodo)
+
+    def startMap(self, width, height):
+         #inicia a matriz
+        line = []
+        for x in range(width):
+            line.append(' ')
+
+        for y in range(height):
+            self.mapa.append(line)
+
+        self.printMap(height)
+        
+       
+
+    def printMap(self, height):
+        print('\n')
+        for x in range(height):
+            print(self.mapa[x])
+
+    def createMap(self, width, height):
+        nextRoom  = 0
+
+        self.startMap(width, height)
+
+        doorP = width // 2
+
+        self.mapa[0][doorP] = 'p'
+        self.printMap(height)
+
+        print('\n')
+
+        
+
+
+
+
 
 class Comodo:
     def __init__(self, tipo, altura, largura):
@@ -83,14 +125,16 @@ def sorteiaTamanhoComodo(comodo):
         if alturaSize*larguraSize >= minS and alturaSize*larguraSize <= maxS:
             return alturaSize, larguraSize
    
-
+  
 
 
 def geraPopInicial():
     pop = []
     casa1 = Casa()
     #Guarda quanto de tamanho o andar ainda tem disponível
-    tamRestante = casa1.tamanho
+    tamRestante = casa1.width * casa1.height
+
+    print(casa1.width, casa1.height)
 
     #randomiza a ordem de inserir os comodos
     print(f'tamRestante {tamRestante}')
@@ -104,27 +148,32 @@ def geraPopInicial():
         a, h = sorteiaTamanhoComodo(comodosAdd[i])
         novoComodo = Comodo(comodosAdd[i], a, h)
         andar1.insereComodo(novoComodo)
-
-
     
     #print comodos
     print(f"####### {andar1.nome} #######")
     for i in range(len(andar1.comodos)):
         print(andar1.comodos[i].tipo, andar1.comodos[i].altura, andar1.comodos[i].largura)
 
-def drawMap():
-    altura = 17
-    largura = 10
-    for i in range(0,largura):
-        print('# ', end='')
-        if i == largura - 1:
-            print()
-            #espaços com o tem que ser feito com um for
-            for j in range(1, altura -1):
-                print('#                 #')
-            for i in range(0,largura):
-                print('# ', end='')
+    andar1.createMap(casa1.width, casa1.height)
 
 
 
-drawMap()
+
+geraPopInicial()
+
+# def drawMap():
+#     altura = 8
+#     largura = 9
+#     for i in range(0,largura):
+#         print('# ', end='')
+#         if i == largura - 1:
+#             print()
+#             #espaços com o tem que ser feito com um for
+#             for j in range(1, altura -1):
+#                 print('#                 #')
+#             for i in range(0,largura):
+#                 print('# ', end='')
+
+
+
+# drawMap()
