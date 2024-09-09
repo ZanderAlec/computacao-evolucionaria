3bba9af355363ed53e3b9e109982c787a66ecbf7
José Alexandre da Silva França <alexandrefranca208@gmail.com>
2024-09-09 06:46:13 -0300

    calculo de fitnesse e mutação

--- projeto/planta.py
+++ projeto/planta.py
@@ -16,26 +16,62 @@ simbols = dict(
     banheiro = ComodosInfo("B", 3, 8),
     corredor = ComodosInfo("*", 2,2),
     escada = ComodosInfo("e", 4, 4),
-    salaDeJantar = ComodosInfo("SJ", 15, 20)
+    salaDeJantar = ComodosInfo("SJ", 15, 20),
+    areaServico = ComodosInfo('AS', 6, 10),
+    closet = ComodosInfo('Cl', 3, 4),
+    quarto = ComodosInfo('Q', 12, 30),
+    ginastica = ComodosInfo('G', 20, 30)
 )
 
 
 
 
 class Casa:
-    def __init__(self, width = 8, height = 9):
+    def __init__(self, width = 22, height = 5):
         #Armazena os andares
         self.andares = []
         self.width = width
         self.height = height
+        self.fitness = 0
 
-    def cria_andar(self, nome, listaComodos):
-        novoAndar = Andar(nome,listaComodos)
-        self.andares.append(novoAndar)
+    def printFloors(self):
+        for floor in self.andares:
+            print(f"####### {floor.nome} #######")
+            for room in floor.comodos:
+                room.print()
+
+    def calcFitness(self):
+        fitness = 0
+        spaceRemaining = 1
+        for i in range(len(self.andares)):
+            for comodo in self.andares[i].comodos:
+                if i == 0: 
+                    if comodo.tipo == 'areaServico' or comodo.tipo == 'banheiro':
+                        fitness+= 10
+                
+                elif i == 1: 
+                    if comodo.tipo == 'quarto' or comodo.tipo == 'closet':
+                        fitness+= 10
+
+                elif i == 2:
+                    if comodo.tipo == 'areaServico':
+                        fitness+= 10
+
+            
+        for i in range(0,2):
+            spaceRemaining += calcRemaningSpace(self.andares[i], self.width, self.height)
+
+        bonusSpaceUsed = 0 if spaceRemaining == 0 else 10 / spaceRemaining
+        fitness += bonusSpaceUsed
+        self.fitness = fitness
+
+        
 
     def generateHouse(self):
         for i in range(len(self.andares)):
             self.andares[i].createMap(self.width, self.height)
+
+    
         
 
 class Andar:
@@ -47,27 +83,41 @@ class Andar:
         #Como os cômodos foram posicionados
         self.mapa= []
 
+        
+    def insertRoom(self, type, width, height):
+        newRoom = Comodo(type, width, height)
+        self.comodos.append(newRoom)
 
-    def insereComodo(self, comodo):
-        self.comodos.append(comodo)
-
-    def startMap(self, width, height):
-         #inicia a matriz
-        line = []
-        for x in range(width):
-            line.append(' ')
-
-        for y in range(height):
-            self.mapa.append(line)
-
+    def startMap(self, width, height):  
+        # Inicializa a matriz mapa com espaços vazios  
+        self.mapa = [['0' for _ in range(width)] for _ in range(height)]  
         self.printMap(height)
         
-       
-
     def printMap(self, height):
         print('\n')
-        for x in range(height):
-            print(self.mapa[x])
+        for linha in self.mapa:  
+            print(' '.join(linha))  
+
+    # def insertRoom(self,room, width, heigth):
+    #     largura = room.largura
+    #     altura = room.altura
+
+    #     larg = False
+    #     #1 - altura
+    #     #2 - largura
+    #     for x in range(heigth):
+    #         for y in range(width):
+    #             if(self.mapa[x][y] != '0' and self.mapa[x][y + 1] == '0'):
+    #                 if(largura - y != '0'):
+    #                     break
+    #             elif y == largura:
+    #                 larg = True
+    #             else  :
+    #                 self.mapa[x][y] = 'c'
+
+    #     self.printMap(heigth)
+
+
 
     def createMap(self, width, height):
         nextRoom  = 0
@@ -81,40 +131,91 @@ class Andar:
 
         print('\n')
 
+        self.insertRoom(self.comodos[0], width, height)
+
         
-
-
-
-
-
 class Comodo:
-    def __init__(self, tipo, altura, largura):
+    def __init__(self, tipo, largura, altura):
         self.tipo = tipo
         self.altura = altura
         self.largura = largura
 
+    def print(self):
+        print(f'{self.tipo}, {self.altura}, {self.largura}')
 
-#Como só tem um andar, a função apenas embaralha
-#No futuro ela deverá determinar quais comodos farão parte de qual andar
-def sorteiaComodos():
-    #A idéia é que todos os andares possíveis estejam aqui
-    #Assim, poderemos sortear qual cômodo vai em cada andar
-    comodos = list(simbols.keys())
-    comodos.remove('corredor')
 
-    #embaralha os comodos
-    shuffle(comodos)
 
-    return comodos
+#Calcula quanto espaço da área total de uma andar foi ocupada por quartos
+def calcRemaningSpace(andar, totalWidth, totalHeight):
+
+    totalm2 = totalWidth * totalHeight
+    for room in andar.comodos:
+        roomm2 = room.altura * room.largura
+        totalm2 -= roomm2
+
+    return totalm2
+
+
+#Preenche todos os andares da casa com comodos
+def sorteiaComodos(casa):
+
+    #Comodos obrigatórios do térreo
+    RoomsT = ['sala', 'cozinha', 'escada', 'salaDeJantar']
+    #Quartos que não foram adicionados no terreo
+    remainingRooms = [x for x in list(simbols.keys()) if x not in RoomsT and x != 'corredor']
+    remainingRooms.extend(['quarto'] * 2)
+
+    shuffle(remainingRooms)
+
+    #Sorteia os valores do térreo
+    for roomName in RoomsT:
+        width, height = drawRoomsSize(roomName) 
+        casa.andares[0].insertRoom(roomName, width, height)
+
+    remainingSpaceT = calcRemaningSpace(casa.andares[0], casa.width, casa.height)
+    remainingSpace1 =  casa.width * casa.height
+
+
+
+    #enquanto a lista não está vazia vai distribuindo os comodos pelos andares
+    while(remainingRooms):
+
+        roomWidth, roomHeight = drawRoomsSize(remainingRooms[0]) 
+        roomSize = roomWidth * roomHeight
+        floor = 0
+
+        #se ambos os quartos tem espaço pra colocar, sorteia quem recebe o quarto
+        if remainingSpaceT >= roomSize and remainingSpace1 >= roomSize :
+            #chance da laje receber area de serviço
+            if(remainingRooms[0] == 'areaServico'):
+                floor = randint(0,2)
+            else:
+                floor = randint(0,1)
+
+        elif remainingSpaceT >= roomSize and remainingSpace1 < roomSize:
+            floor = 0
+            
+        elif remainingSpace1 >= roomSize and remainingSpaceT < roomSize:
+            floor = 1
+            
+
+        if floor == 0:
+            remainingSpaceT -= roomSize
+        elif floor == 1:
+            remainingSpace1 -= roomSize
+
+        casa.andares[floor].insertRoom(remainingRooms[0], roomWidth, roomHeight)
+        remainingRooms.pop(0) 
+
+    casa.andares[1].insertRoom('escada', 4,4)
+    casa.andares[2].insertRoom('escada', 4,4)
     
-
-#retorno valores aletórias da latura e largura
-#Acredito que o programa ja calculou 68 como minimo pro max de tudo
-def sorteiaTamanhoComodo(comodo):
+#retorno valores aletórias da largura e altura
+def drawRoomsSize(comodo):
 
     minS = simbols[comodo].minSize
     maxS = simbols[comodo].maxSize
-
+    
     if minS == maxS:
         return int(minS/2), int(maxS/2)
     
@@ -127,82 +228,96 @@ def sorteiaTamanhoComodo(comodo):
    
   
 
-
 def geraPopInicial():
-    pop = []
-    casa1 = Casa()
-    #Guarda quanto de tamanho o andar ainda tem disponível
-    tamRestante = casa1.width * casa1.height
 
-    print(casa1.width, casa1.height)
+    for i in range(0, popSize):
+        casa = Casa()
 
-    #randomiza a ordem de inserir os comodos
-    print(f'tamRestante {tamRestante}')
-    comodosAdd = sorteiaComodos()
-    print(comodosAdd)
-
-    andar1 = Andar('Térreo')
-
-    #randomizar o tamanho dos comodos
-    for i in range(len(comodosAdd)):
-        a, h = sorteiaTamanhoComodo(comodosAdd[i])
-        novoComodo = Comodo(comodosAdd[i], a, h)
-        andar1.insereComodo(novoComodo)
+        #inicializa os andares
+        casa.andares = [Andar('Térreo'), Andar('1 Andar'), Andar('Laje')]
     
-    #print comodos
-    print(f"####### {andar1.nome} #######")
-    for i in range(len(andar1.comodos)):
-        print(andar1.comodos[i].tipo, andar1.comodos[i].altura, andar1.comodos[i].largura)
+        #preenche os andares da casa com comodos aleatórios
+        sorteiaComodos(casa)
+        casa.calcFitness()
+        pop.append(casa)
+    
 
-    andar1.createMap(casa1.width, casa1.height)
+def printPop():
+    for i in range(popSize):
+        print('\n')
+        pop[i].printFloors()
+        print(f'\n{pop[i].fitness}')
+        
 
-
-
-
-geraPopInicial()
-
-# def drawMap():
-#     altura = 8
-#     largura = 9
-#     for i in range(0,largura):
-#         print('# ', end='')
-#         if i == largura - 1:
-#             print()
-#             #espaços com o tem que ser feito com um for
-#             for j in range(1, altura -1):
-#                 print('#                 #')
-#             for i in range(0,largura):
-#                 print('# ', end='')
+def getFitness(casa):
+    return casa.fitness
 
 
+#muta todo mundo
+def selectParentes():
+    for i in range(len(pop)):
+        mutate(pop[i])
 
-# drawMap()
 
-class Mapa:  
-    def __init__(self):  
-        self.mapa = []  
+def mutate(casa):
+    
+    #muta os andares---------------------------
+    #Quartos que não podem ser mutados
+    fixedT = ['sala', 'cozinha', 'escada', 'salaDeJantar']
+    remaingT = [x for x in casa.andares[0].comodos if x.tipo not in fixedT]
 
-    def startMap(self, width, height):  
-        # Inicializa a matriz mapa com espaços vazios  
-        self.mapa = [[' ' for _ in range(width)] for _ in range(height)]  
+    if len(remaingT) != 0:
+        remaing1F = casa.andares[1].comodos
 
-    def printMap(self, height):  
-        for linha in self.mapa:  
-            print(' '.join(linha))  
+        rt = randint(0,  len(remaingT) - 1) if len(remaingT) > 1 else 0
+        r1 = randint(0,  len(remaing1F) - 1)
 
-    def createMap(self, width, height):  
-        nextRoom = 0  
+        roomt = casa.andares[0].comodos[rt]
+        room1 = casa.andares[1].comodos[r1]
 
-        self.startMap(width, height)  
+        #Realizar a troca
+        casa.andares[0].comodos.pop(rt)
+        casa.andares[1].comodos.pop(r1)
 
-        doorP = width // 2  
+        casa.andares[0].comodos.insert(r1, room1)
+        casa.andares[1].comodos.insert(rt, roomt)
 
-        # Coloca 'p' na posição específica  
-        self.mapa[0][doorP] = 'p'  
-        self.printMap(height)  
+    #muta o tamanho dos comodos ----
+    for i in range(0,2):
+        rand = randint(1, 100)
+        if rand >= 50:
+            rand = randint(0, len(casa.andares[i].comodos) - 1)
+            #Só muda o valor do cômodo se ele couber  
+            space = calcRemaningSpace(casa.andares[i], casa.width, casa.height)
+            newW, newH =  drawRoomsSize(casa.andares[i].comodos[rand].tipo)
+            if(space >= newW * newH):
+                casa.andares[i].comodos[rand].altura, casa.andares[i].comodos[rand].altura = newW, newH
 
-        print('\n')  
+    casa.calcFitness()
 
-# Exemplo de uso  
-mapa = Mapa()  
-mapa.createMap(5, 3)  
+
+
+
+
+pop = []
+popSize = 10
+geracoes = 10
+
+def main():
+    geraPopInicial()
+    printPop()
+    pop.sort(key = getFitness, reverse = True)
+
+
+    #criar um critério de seleção de pais melhor
+    #desenhar as casas
+    for i in range(0, geracoes):
+        selectParentes()
+        pop.sort(key = getFitness, reverse = True)
+        printPop()
+
+
+
+if __name__ == "__main__":
+    main()
+
